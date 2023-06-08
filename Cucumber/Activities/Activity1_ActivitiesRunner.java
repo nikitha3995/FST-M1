ed1986ba283f572d4c312c08f52ba3315f047e00
nikitha3995 <128298945+nikitha3995@users.noreply.github.com>
2023-06-08 16:01:36 +0530

    Add files via upload

---
+++ Cucumber/Activities/Activity1_ActivitiesRunner.java
@@ -0,0 +1,15 @@
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@activity1"
+)
+
+public class Activity1_ActivitiesRunner {
+    //empty
+}
---
+++ Cucumber/Activities/Activity5_ActivitiesRunner.java
@@ -0,0 +1,9 @@
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@activity5"
+)
+public class Activity5_ActivitiesRunner {
+
+}
